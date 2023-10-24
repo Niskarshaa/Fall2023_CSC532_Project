8d1a06c83db92024e0059f0ae3d6b19c3242d7e0
Ramon Mufutau <amufutau@unilag.edu.mg>
2023-10-24 10:40:05 -0700

    new

---
+++ Project/Fall2020_CSC403_Project/frmFont.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Fall2020_CSC403_Project
+{
+    partial class frmFront
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(450, 162);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(519, 520);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(410, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(350, 712);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(1550, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Game";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(1326, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 62);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Game Instruction";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // frmFront
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1743, 794);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Name = "frmFront";
+            this.Text = "frmFront";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmFront_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Project/Fall2020_CSC403_Project/frmFont.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fall2020_CSC403_Project
+{
+    public partial class frmFront : Form
+    {
+        private List<Image> images = new List<Image>();
+        private List<string> label1Text = new List<string>();
+        private List<string> label2Text = new List<string>();
+        private int currentImageIndex = 0;
+        public frmFront()
+        {
+            InitializeComponent();
+        }
+
+        private void frmFront_Load(object sender, EventArgs e)
+        {
+            // Set the background color of the form to a specific color
+            //this.BackColor = Color.YellowGreen;
+            label1.Font = new Font(label1.Font.FontFamily, 16, label1.Font.Style); // Replace 16 with your desired font size
+            label2.Font = new Font(label1.Font.FontFamily, 12, label1.Font.Style); // Replace 16 with your desired font size
+
+
+            images.Add(Properties.Resources.player); // Load images from resources
+            images.Add(Properties.Resources.enemy_koolaid); 
+            images.Add(Properties.Resources.enemy_cheetos);
+            images.Add(Properties.Resources.enemy_poisonpacket);
+            // Add more images as needed
+
+            label1Text.Add("Main Player");
+            label1Text.Add("enemy_koolaid");
+            label1Text.Add("enemy_cheetos");
+            label1Text.Add("enemy_poisonpacket");
+            // Add more text as needed
+
+            label2Text.Add("Player Character (Your Avatar): \n A young hero with the ability to harness the power of Air. \n You are known for your agility and problem-solving skills.");
+            label2Text.Add("Koolaid (Enemy - representing the corrupted Water Element): \n Koolaid is a devious and manipulative enchantress who \n wants to flood Eldorania with her dark Water magic.");
+            label2Text.Add("Chootos (Enemy - representing the corrupted Fire Element):\n Chootos is a cunning and power-hungry sorcerer who \n seeks to unleash the destructive force of Fire to rule the realm.");
+            label2Text.Add("Poisonpacket (Enemy - representing the corrupted Earth Element): \n Poisonpacket is a treacherous rogue who plans \n to spread chaos by infecting the land with poisonous Earth.");
+            // Add more text as needed
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+              if (currentImageIndex < images.Count)
+            {
+                pictureBox1.Image = images[currentImageIndex];
+                label1.Text = label1Text[currentImageIndex];
+                label2.Text = label2Text[currentImageIndex];
+                currentImageIndex++;
+
+                if (currentImageIndex >= images.Count)
+                {
+                    currentImageIndex = 0; // Reset the image index
+                    timer1.Enabled = true; // Start the timer
+                }
+
+            }
+  
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            FrmLevel frmLevel = new FrmLevel(); // Create an instance of the instruction form
+            frmLevel.ShowDialog(); // Show the instruction form as a modal dialog
+
+           // Environment.Exit(0);
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            this.Hide();
+            FrmHelp frmHelp = new FrmHelp(); // Create an instance of the instruction form
+            frmHelp.ShowDialog(); // Show the instruction form as a modal dialog
+
+            // Environment.Exit(0);
+        }
+    }
+    }
